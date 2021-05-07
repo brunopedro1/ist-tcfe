3fa97b658198644bb27a220898dc9adb224fb528
Bruno Pedro <bmbpedro@gmail.com>
2021-05-07 02:34:07 +0100

    octave e ngspice finais

--- t3/mat/t3.m
+++ t3/mat/t3.m
@@ -13,17 +13,17 @@ v_in = 230
 
 % Transformer n:1
 
-n = 10.20 
+n = 10.7785890358
 
 A = v_in/n
 
 
 % Envelope Detector 
 
-t=linspace(0, 0.1, 1000);
+t=linspace(0, 10/f, 1000);
 
-R_env = 10000  % envelope detector resistance 
-C_env = 10e-6  % envelope detector capacitor
+R_env = 150000  % envelope detector resistance 
+C_env = 100e-6  % envelope detector capacitor
 
 vS_env = A*cos(w*t)
 
@@ -34,6 +34,16 @@ tOFF = 1/w * atan(1/w/R_env/C_env);
 
 vOnexp = A*cos(w*tOFF)*exp(-(t-tOFF)/R_env/C_env);
 
+%Half wave rectifier
+%for i=1:length(t)
+%	  if (vS_env(i) > 0)
+%	    vOhr(i) = vS_env(i);
+%	  else
+%	    vOhr(i) = 0;
+%	  endif
+%endfor
+
+%Full wave rectifier
 for i=1:length(t)
 	  vOhr(i) = abs(vS_env(i));
 endfor
@@ -57,13 +67,17 @@ ripple_env = max(vO_env) - min(vO_env)
 
 % Voltage Regulator
 
+vO_reg = zeros(1, length(t));
+VO_reg = 0;
+vo_reg = zeros(1, length(t));
+
 Vt = 0.026;
-Is = 10e-14;
+Is = 1e-14;
 eta = 1
-Rreg = 1e3
+Rreg = 5000
 
 n_diodes = 18;
-VON = 0.70;
+VON = 12/n_diodes;
 
 Rd = Vt*eta/(Is*exp(VON/(eta*Vt)))
 
@@ -84,25 +98,55 @@ endfor
 
 vO_reg = VO_reg + vo_reg;
 
-hf=figure();
-plot(t*1000, vOhr, t*1000, vO_env, t*1000, vO_reg);
-title("Output voltage");
-xlabel ("t[ms]");
-legend("rectified","envelope", "regulator");
-print (hf,"vO_env&vOhr&vO_reg.eps", "-depsc");
+ripple_reg = max(vO_reg)-min(vO_reg) 
 
+%hf=figure();
+%plot(t*1000, vOhr, t*1000, vO_env, t*1000, vO_reg);
+%title("Output voltage");
+%xlabel ("t[ms]");
+%legend("rectified","envelope", "regulator");
+%print (hf,"vO_env&vOhr&vO_reg.eps", "-depsc");
+
+%hfr=figure();
+%plot(t*1000, vOhr);
+%xlabel ("t [ms]");
+%ylabel ("V [Volts]");
+%legend("V_{rectified}");
+%print (hfr,"vOhr.eps", "-depsc");
+
+hfenv=figure();
+plot(t*1000, vO_env);
+xlabel ("t [ms]");
+ylabel ("V [Volts]");
+legend("V_{envelope}");
+print (hfenv,"vO_env.eps", "-depsc");
+
+hfreg=figure();
+plot(t*1000, vO_reg);
+xlabel ("t [ms]");
+ylabel ("V [Volts]");
+legend("V_{regulator}");
+print (hfreg,"vO_reg.eps", "-depsc");
 
 % Deviations (vO - 12) 
 
 hfc = figure();
 title("Deviations from DC voltage 12V")
-plot (t*1000,vO_reg-12, ";vo-12 (t);");
+plot (t*1000,vO_reg-12);
 xlabel ("t[ms]")
-ylabel ("v_O [Volts]")
-legend('Location','northeast');
+ylabel ("V [Volts]")
+legend('V_{O} - 12 ');
 print (hfc, "deviation.eps", "-depsc");
 
 
+cost = R_env/1000 + Rreg/1000 + C_env*1e6 + n_diodes*0.1+0.4
 
+merit = 1/(cost*(ripple_reg + abs(mean(vO_reg) - 12) + 1e-6))
 
+printf('op_TAB\n');
+printf('n = %f\n$R_{envelope}$  [kOhm] = %f\n$C_{envelope}$  [uF] = %f\n$R_{regulator}$  [kOhm] = %f\n', n, R_env/1000, C_env*1e6, Rreg/1000);
+printf('op_END\n');
 
+printf('op2_TAB\n');
+printf('$Avarage(v_O)$  [V] = %f\n$Ripple(v_O)$  [V] = %f\n$Cost$  [MU] = %f\n$Merit$ = %f\n', mean(vO_reg), ripple_reg , cost, merit);
+printf('op2_END\n');
